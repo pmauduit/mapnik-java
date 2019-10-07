cdbffed838a2ed44dae8509f0bac4e1bf8989a60
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2019-10-07 16:38:55 +0200

    first shot on a france métropolitaine renderer

--- src/main/java/fr/beneth/mapnik/EntryPoint.java
+++ src/main/java/fr/beneth/mapnik/EntryPoint.java
@@ -18,33 +18,31 @@ import mapnik.Renderer;
 public class EntryPoint {
 
     public static void main(String[] args) throws ClassNotFoundException, URISyntaxException, IOException {
-        // Some java black magic to load the interned jar
-        URL mapnikJniJar = EntryPoint.class.getResource("/lib/mapnik-jni.jar");
-        URLClassLoader child = new URLClassLoader(new URL[] { mapnikJniJar }, EntryPoint.class.getClassLoader());
-        Class.forName("mapnik.Mapnik", true, child);
 
         Mapnik.initialize();
-
-        Box2d bounds = new Box2d(5.91478, 45.56237, 5.92904, 45.56921);
+        // France métropolitaine
+        // -9.86 41.15
+        // 10.38 51.56
+        Box2d bounds = new Box2d(-9.86, 41.15, 10.38, 51.56);
         MapDefinition m = new MapDefinition();
 
-        URL myMapFile = EntryPoint.class.getResource("/data/simplemap.xml");
+        URL myMapFile = EntryPoint.class.getResource("/data/france.xml");
         // This should be done by the initialize() call, but ...
         FreetypeEngine.registerFonts(Mapnik.getInstalledFontsDir(), true);
 
         m.loadMap(new File(myMapFile.toURI()).getAbsolutePath(), false);
         m.setSrs(Projection.LATLNG_PARAMS);
-        m.resize(512, 512);
+        m.resize(4096, 4096);
         m.zoomToBox(bounds);
 
-        Image image = new Image(512, 512);
+        Image image = new Image(4096, 4096);
         Renderer.renderAgg(m, image);
 
         byte[] contents = image.saveToMemory("png");
         image.dispose();
         m.dispose();
 
-        FileOutputStream fos = new FileOutputStream("chambery.png");
+        FileOutputStream fos = new FileOutputStream("france.png");
         fos.write(contents);
         fos.flush();
         fos.close();
